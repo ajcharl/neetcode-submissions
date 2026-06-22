5b14ea26d381faff92eef1975b463f24e7716e64
charles <charles_ajie@yahoo.com>
2026-06-22 16:03:31 -0400

    Add: permutations - submission-5

---
+++ Data Structures & Algorithms/permutations/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        subset = []
+
+        def dfs():
+            if len(subset) == len(nums):
+                result.append(subset.copy())
+                return
+
+            for num in nums:
+                if num not in subset:
+                    subset.append(num)
+
+                    dfs()
+
+                    subset.pop()
+        dfs()
+        return result
+    
